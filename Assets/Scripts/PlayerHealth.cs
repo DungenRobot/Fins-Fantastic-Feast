65cbc94c864195836135b9355f22198e49750cd8
speciminx <150974086+speciminx@users.noreply.github.com>
2023-12-04 18:17:46 -0500

    iFrame start
    working on giving the player Invulnerability after damage

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -6,7 +6,10 @@ using UnityEngine.SceneManagement;
 public class PlayerHealth : MonoBehaviour
 {
     public int cheese = 0;
-
+    public float iTime; //how long the player is invulnerable in seconds
+    private void Update() {
+        
+    }
     void getCheese(int amt) { cheese+=amt; }
     void takeDamage(uint amt) {
         if (cheese < amt) {
