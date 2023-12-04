512fb00ec125a6cc350052f7556647f371c292ef
speciminx <150974086+speciminx@users.noreply.github.com>
2023-12-04 13:30:29 -0500

    Bat.
    added the bat
    the bat is unfinished
    the bat is consuming my sanity

--- Assets/Scripts/BatController.cs
+++ Assets/Scripts/BatController.cs
@@ -2,16 +2,144 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BatController : MonoBehaviour
-{
-
-    public float offset = 1; // how far the bat will deviate from a straight line when diving
+public class BatController : MonoBehaviour{
+    private enum batState {
+        SLEEPING,
+        WAITING,
+        DESCENDING,
+        ATTACKING,
+        ASCENDING
+    }; // used to determine what the bat should be doing
+    private Vector3 startPos, targetPos1, targetPos2, endPos;//positions of the path
+    private Transform selfPos;
+    public float unitsPerSecond; //on average how fast the bat travels in units per second
+    public float delay; //how many seconds from detection until attack
+    private float timer;//used for waiting
+    private float between = 0;//used for LERPing
+    public float LPS1, LPS2, LPS3; //used for LERPing between points in the path traveled
+    private batState batMode = batState.SLEEPING; //used for saving what state the bat is in
     void Start(){
-        
+        //defining positions
+        selfPos = GetComponent<Transform>();
+        startPos = selfPos.position;
+        endPos = startPos;
+        BoxCollider2D scaler = GetComponentInChildren<BoxCollider2D>();
+        Transform hitPos = scaler.GetComponent<Transform>();
+        endPos.x += (hitPos.position.x - startPos.x) * 2;
+        targetPos1 = hitPos.position;
+        targetPos2 = hitPos.position;
+        targetPos1.x -= scaler.size.x / 2;
+        targetPos2.x += scaler.size.x / 2;
+        //getting path/speed info
+        float dist1 = dist(startPos, targetPos1);
+        float dist2 = dist(targetPos1,targetPos2);
+        float dist3 = dist(targetPos2, endPos);
+        LPS1 = dist1 / unitsPerSecond;
+        LPS2 = dist2 / unitsPerSecond;
+        LPS3 = dist3 / unitsPerSecond;
+        //setting other things
+        timer = delay;
     }
 
     // Update is called once per frame
     void Update(){
-        
+        switch (batMode) {
+            case batState.SLEEPING: //skips entire update if bat is doing nothing
+                return;
+            case batState.WAITING:
+                behavior_WAITING();
+                break;
+            case batState.DESCENDING:
+                behavior_DESCENDING();
+                break;
+            case batState.ATTACKING:
+                behavior_ATTACKING();
+                break;
+            case batState.ASCENDING:
+                behavior_ASCENDING();
+                break;
+        }
+    }
+
+    private void behavior_WAITING() {
+        if (timer <= 0) {
+            timer = delay;
+            batMode = batState.DESCENDING;
+        } else {
+            timer -= Time.deltaTime;
+        }
+    }
+
+    private void behavior_DESCENDING() {
+        between += LPS1 * Time.deltaTime;
+        Vector3 nextPos;
+        if (between < 1) {
+            nextPos = new Vector3 {
+                x = Mathf.Lerp(startPos.x, targetPos1.x, between),
+                y = Mathf.Lerp(startPos.y, targetPos1.y, LerpSmooth(between)),
+                z = startPos.z
+            };
+        } else {
+            between = 0;
+            nextPos = targetPos1;
+            batMode = batState.ATTACKING;
+        }
+        selfPos.position = nextPos;
+    }
+
+    private void behavior_ATTACKING() {
+        between += LPS2 * Time.deltaTime;
+        Vector3 nextPos;
+        if (between < 1) {
+            nextPos = new Vector3 {
+                x = Mathf.Lerp(targetPos1.x, targetPos2.x, between),
+                y = targetPos1.y,
+                z = targetPos1.z
+            };
+        } else {
+            between = 0;
+            nextPos = targetPos2;
+            batMode = batState.ASCENDING;
+        }
+        selfPos.position = nextPos;
+    }
+
+    private void behavior_ASCENDING() {
+        between += LPS3 * Time.deltaTime;
+        if (between < 1) {
+            Vector3 nextPos = new Vector3 {
+                x = Mathf.Lerp(targetPos2.x, endPos.x, between),
+                y = Mathf.Lerp(targetPos2.y, endPos.y, LerpSmooth(between)),
+                z = targetPos2.z
+            };
+            selfPos.position = nextPos;
+        } else {
+            between = 0;
+            returnToStart();
+        }
+    }
+
+    private void returnToStart() {//its named this instead of Reset because Reset is already taken
+        batMode = batState.SLEEPING;
+        selfPos.position = startPos;
+    }
+
+    void PlayerDetected() {
+        batMode = (batMode == batState.SLEEPING) ? batState.WAITING : batMode;//activates bat if bat is not already active, otherwise just assigns the value of batMode to itself
+    }
+    
+    private float LerpSmooth(float value) {
+        float rVal = Mathf.Clamp(value, 0, 1);
+        switch (batMode) {
+            case batState.DESCENDING:
+                return Mathf.Sin((Mathf.PI / 2) * value);
+            case batState.ASCENDING:
+                return (1 - Mathf.Cos((Mathf.PI / 2) * value));
+        }
+        return value;
+    }// used to make swooping path smoother
+
+    private float dist(Vector3 v1, Vector3 v2) {//used my own because Vector3.distance wasn't giving correct results
+        return Mathf.Sqrt(Mathf.Pow(v1.x - v2.x, 2) + Mathf.Pow(v1.y - v2.y, 2) + Mathf.Pow(v1.z - v2.z, 2));
     }
 }
